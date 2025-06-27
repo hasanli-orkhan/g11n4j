140638f061d52f7fb0d7802f92780ed8e634fac5
hasanli-orkhan <orkhan.hasanli.92@gmail.com>
2025-06-27 21:05:36 +0400

    chore: define classes for supporting Gettext, JSON, NoSQL, RDBMS, TMX, XLIFF extensions. (need to be implemented)

---
+++ g11n4j-core/src/main/java/info/md7/g11n4j/core/source/JsonMessageSource.java
@@ -0,0 +1,20 @@
+package info.md7.g11n4j.core.source;
+
+import info.md7.g11n4j.core.exception.NoSuchMessageException;
+
+import java.util.Locale;
+import java.util.Map;
+
+public class JsonMessageSource implements MessageSource {
+
+    @Override
+    public String getMessage(String key, Locale locale) throws NoSuchMessageException {
+        // todo implement this
+        throw new RuntimeException("Not yet implemented.");
+    }
+
+    @Override
+    public Map<String, String> getPluralForms(String keyPrefix, Locale locale) {
+        return Map.of();
+    }
+}
