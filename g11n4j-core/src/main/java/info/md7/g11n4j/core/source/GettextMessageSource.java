d2aa07edd6461c1be5c589ef55c2b3c8d5417b7e
hasanli-orkhan <orkhan.hasanli.92@gmail.com>
2025-07-01 01:43:20 +0500

    chore: implement methods for other sources

--- g11n4j-core/src/main/java/info/md7/g11n4j/core/source/GettextMessageSource.java
+++ g11n4j-core/src/main/java/info/md7/g11n4j/core/source/GettextMessageSource.java
@@ -1,6 +1,7 @@
 package info.md7.g11n4j.core.source;
 
 import info.md7.g11n4j.core.exception.NoSuchMessageException;
+import info.md7.g11n4j.core.i18n.MessageContext;
 
 import java.util.Locale;
 import java.util.Map;
@@ -11,6 +12,16 @@ public class GettextMessageSource implements MessageSource {
     public String getMessage(String key, Locale locale) throws NoSuchMessageException {
         // todo implement this
         throw new RuntimeException("Not yet implemented.");
+    }
+
+    @Override
+    public String getMessage(String key, Locale locale, MessageContext context) throws NoSuchMessageException {
+        return "";
+    }
+
+    @Override
+    public Map<String, String> getPluralForms(String keyPrefix, Locale locale, MessageContext context) {
+        return Map.of();
     }
 
     @Override
--- g11n4j-core/src/main/java/info/md7/g11n4j/core/source/XliffMessageSource.java
+++ g11n4j-core/src/main/java/info/md7/g11n4j/core/source/XliffMessageSource.java
@@ -1,6 +1,7 @@
 package info.md7.g11n4j.core.source;
 
 import info.md7.g11n4j.core.exception.NoSuchMessageException;
+import info.md7.g11n4j.core.i18n.MessageContext;
 
 import java.util.Locale;
 import java.util.Map;
@@ -11,6 +12,16 @@ public class XliffMessageSource implements MessageSource {
     public String getMessage(String key, Locale locale) throws NoSuchMessageException {
         // todo implement this
         throw new RuntimeException("Not yet implemented.");
+    }
+
+    @Override
+    public Map<String, String> getPluralForms(String keyPrefix, Locale locale, MessageContext context) {
+        return Map.of();
+    }
+
+    @Override
+    public String getMessage(String key, Locale locale, MessageContext context) throws NoSuchMessageException {
+        return "";
     }
 
     @Override
