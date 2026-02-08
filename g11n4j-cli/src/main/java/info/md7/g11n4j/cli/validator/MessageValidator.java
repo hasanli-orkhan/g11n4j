49674e1865f63d5e39ed6fb01b772c48af2ba38a
hasanli-orkhan <orkhan.hasanli.92@gmail.com>
2026-02-09 02:54:04 +0400

    refactor: remove g11n4j-cli module and all associated classes, tests, and resources

--- g11n4j-cli/src/main/java/info/md7/g11n4j/cli/validator/MessageValidator.java
+++
@@ -1,8 +0,0 @@
-package info.md7.g11n4j.cli.validator;
-
-import java.util.List;
-
-public interface MessageValidator {
-
-    boolean validate(String defaultLocale, List<String> compareLocales);
-}
